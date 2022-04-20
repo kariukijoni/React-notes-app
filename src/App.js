d57fdf39fa041ed1c1bff9836e11a6982e5baa05
kariukijoni <kariukijoni@gmail.com>
2022-04-20 12:55:53 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+ 
+ import React from "react";
+ import {
+   BrowserRouter as Router,
+   Route,
+   Routes,
+ } from "react-router-dom";
+ import './App.css';
+ import Header from './components/Header.js'
+ import NotesListPage from './pages/NotesListPage';
+ import NotePage from './pages/NotePage';
+
+function App() {
+  return (
+  <Router>
+   <div className="container">
+    <div className="app">
+          <Header/>
+          <Routes>
+                  <Route path='/' element={<NotesListPage/>}/>
+                  <Route path='/note/:id' element={<NotePage/>}/>
+          </Routes>
+
+    </div>
+   </div>
+  </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/NotePage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import notes from '../assets/data'
+import { useParams } from 'react-router-dom';
+
+
+
+ const NotePage = () => {
+    const {id} = useParams();
+    const note = notes.find(note => note.id === Number(id))
+  return (
+    <div>
+        <p>{note?.body}</p>
+    </div>
+  )
+}
+
+export default NotePage;
