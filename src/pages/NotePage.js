81467f142b20330476f08eae4f1a86ae285740b5
kariukjoni <kariukijoni@gmail.com>
2022-04-20 17:46:13 +0300

    Making API calls

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,6 +1,6 @@
-import React from 'react'
 
-import notes from '../assets/data'
+import React,{useState,useEffect} from 'react'
+// import notes from '../assets/data'
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
@@ -9,7 +9,19 @@ import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
 
  const NotePage = () => {
     const {id} = useParams();
-    const note = notes.find(note => note.id === Number(id))
+    // const note = notes.find(note => note.id === Number(id))
+    let[note,setNote]=useState(null)
+    
+    useEffect(()=>{
+        getNote()
+    },[id])
+    
+    let getNote=async()=>{
+        let response=await fetch(`http://localhost:8000/notes/${id}`)
+        let data=await response.json()
+        setNote(data)
+    }
+    
   return (
     <div className='note'>
         <div className='note-header'>
